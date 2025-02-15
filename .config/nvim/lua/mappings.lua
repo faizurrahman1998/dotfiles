eba06f1713e7f8a54b8476aa61e9f5c1a4f75f28
fayez <faizurrahman.fayez@gmail.com>
2025-02-16 02:29:33 +0600

    main: configured NvChad

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,15 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+local options = {
+    noremap = true,
+    silent = true,
+}
+
+map("i", "jk", "<ESC>")
+map("n", "<CR>", ":noh<CR>", options)
+
+-- neogen keymaps
+map("n", "<leader>Nff", "<cmd>Neogen file<CR>", { desc = "Generate Doxygen comment of type file." })
+map("n", "<leader>Nfn", "<cmd>Neogen func<CR>", { desc = "Generate Doxygen comment of type func." })
+map("n", "<leader>Nfc", "<cmd>Neogen class<CR>", { desc = "Generate Doxygen comment of type class" })
