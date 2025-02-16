d99bde246b35ad49dbe0f6f55d36a9e4f176223b
fayez <faizurrahman.fayez@gmail.com>
2025-02-16 18:09:53 +0600

    main:

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -6,7 +6,8 @@ return {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
-        }
+        },
+        lazy = false
     },
 
     {
