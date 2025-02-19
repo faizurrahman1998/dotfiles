5c8559cdedb4a7c0971fdbb4666d24ff3dc3425d
fayez <faizurrahman.fayez@gmail.com>
2025-02-20 03:39:22 +0600

    main: added persistence.nvim to manage sessions.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -19,3 +19,16 @@ map("n", "<CR>", ":noh<CR>", options)
 map("n", "<leader>Nff", "<cmd>Neogen file<CR>", { desc = "Generate Doxygen comment of type file." })
 map("n", "<leader>Nfn", "<cmd>Neogen func<CR>", { desc = "Generate Doxygen comment of type func." })
 map("n", "<leader>Nfc", "<cmd>Neogen class<CR>", { desc = "Generate Doxygen comment of type class" })
+
+-- persistence keymaps
+-- load the session for the current directory
+map("n", "<leader>qs", function() require("persistence").load() end, { desc = "Load the session for the current directory."})
+
+-- select a session to load
+map("n", "<leader>qS", function() require("persistence").select() end, { desc = "Select a session to load."})
+
+-- load the last session
+map("n", "<leader>ql", function() require("persistence").load({ last = true }) end, { desc = "Load the last session."})
+
+-- stop Persistence => session won't be saved on exit
+map("n", "<leader>qd", function() require("persistence").stop() end, { desc = "Stop Persistence"})
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,4 +1,13 @@
 return {
+    -- Lua
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre", -- this will only start session saving when an actual file was opened
+        opts = {
+            -- add any custom options here
+        }
+    },
+
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
